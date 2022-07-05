d7a6da3a9aa8684e7ebc2f72f4305c1809bbca85
Kevin <klattkev@gmail.com>
2022-07-06 01:59:19 +0800

    first

---
+++ index.js
@@ -0,0 +1,47 @@
+import * as THREE from 'three';
+import metaversefile from 'metaversefile';
+const {useApp, useFrame, useLoaders, usePhysics, useCleanup} = metaversefile;
+
+const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
+
+export default () => {
+  const app = useApp();
+  const physics = usePhysics();
+
+  app.name = 'city';
+
+  useFrame(() => {
+
+  });
+
+  let physicsIds = [];
+  (async () => {
+    const u = `${baseUrl}assets/ground.glb`;
+    let o = await new Promise((accept, reject) => {
+      const {gltfLoader} = useLoaders();
+      gltfLoader.load(u, accept, function onprogress() {}, reject);
+    });
+    o = o.scene;
+    app.add(o);
+    
+    const physicsId = physics.addGeometry(o);
+    physicsIds.push(physicsId);
+  })();
+  (async () => {
+    const u = `${baseUrl}assets/city.glb`;
+    let o = await new Promise((accept, reject) => {
+      const {gltfLoader} = useLoaders();
+      gltfLoader.load(u, accept, function onprogress() {}, reject);
+    });
+    o = o.scene;
+    app.add(o);
+  })();
+  
+  useCleanup(() => {
+    for (const physicsId of physicsIds) {
+      physics.removeGeometry(physicsId);
+    }
+  });
+
+  return app;
+};
